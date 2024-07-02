23b906171cc903825a246fd427c0af69f40c5a5b
Aya Fergany <ayafergany8@gmail.com>
2024-07-02 17:18:17 +0300

    code/gemini-pro

---
+++ gemini_pro.py
@@ -0,0 +1,11 @@
+import os
+import streamlit as st
+import google.generativeai as genai
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+os.environ['GOOGLE_API_KEY'] = "AIzaSyBTm4axDgCGC3Vc0ps8EMMnm7MDE0ZbjAM"
+genai.configure(api_key = os.environ['GOOGLE_API_KEY'])
+
+llm = ChatGoogleGenerativeAI(model="gemini-pro")
+response = llm.invoke("Explain Quantum Computing in 50 words?")
+print(response.content)
